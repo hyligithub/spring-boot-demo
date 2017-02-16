2e73fcbc9f43a202bd469dcbb15f81042688a05b
lihuiyan <lihuiyan@jd.com>
2017-02-16 18:36:23 +0800

    example

---
+++ src/main/java/com/example/Example.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by lihuiyan on 2017/2/16.
+ */
+@RestController
+@EnableAutoConfiguration
+public class Example {
+    @RequestMapping("/")
+    String home() {
+        return "hello world";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Example.class, args);
+    }
+}
